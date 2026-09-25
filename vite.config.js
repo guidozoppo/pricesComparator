121719f3ede3abf2cdcc9dd0cf5c83113beca054
guidozoppo <1310.guido@gmail.com>
2026-09-24 21:19:29 -0300

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 casaJonas: resolve(__dirname, 'src/front/casa-jonas.html'),
-                tpa: resolve(__dirname, 'src/front/tpa.html')
+                tpa: resolve(__dirname, 'src/front/tpa.html'),
+                mali: resolve(__dirname, 'src/front/mali.html')
             }
         }
     }
